ae09c7d7847664e4613d6b62fccb25f784a0dacd
Jrscodes <thelions20k@icloud.com>
2021-10-01 14:11:14 -0600

    Initial Commit

---
+++ ViVe/ViVeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViVeApp.swift
+//  ViVe
+//
+//  Created by Abraham on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ViVeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
